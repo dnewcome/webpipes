b03bd555e117d6fd05fe8ded107c0d02b1d6d6b5
dannimda <dannimda@localhost.(none)>
2010-04-30 22:42:12 +0000

    adding convenience script for server and smoke test for wp command

---
+++ runtests.sh
@@ -0,0 +1,9 @@
+# correct output 
+#
+#[ { id: 'ep', added: '2003-06-10' }
+#, { id: 'tse', added: '2003-06-20' }
+#, { id: 'lh', added: '2004-11-01' }
+#, { id: 'co', added: '2004-11-15' }
+#]
+
+echo '[ "//label", { id: "@id", added: "@added" } ]' | ./wp http://localhost:9980/?src=http://localhost/labels.xml

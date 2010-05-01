063e8e06b202309c7b6ef57d7e3b62651e8d65f5
dannimda <dannimda@localhost.(none)>
2010-05-01 05:47:58 +0000

    chaining working for chains of two in a pipeline, still need to write code to forward on the rest of the pipe if there is any

--- server.js
+++ server.js
@@ -18,7 +18,29 @@ http.createServer( function( req, res ) {
 	sys.puts( 'request url is: ' + req.url );
 
 	res.writeHead( 200 )
-
+	var commands = getCommands( req.url ); 
+	sys.puts( '--- commands: ' + commands );
+	if( commands ) {
+		// todo: need to forward on the rest of the pipe if
+		// there is one 
+		var pipeurl = commands[0];
+		sys.puts( 'opening request to next in pipe: ' + pipeurl );			
+		// we need to connect to the next node in the pipeline
+		var outrequest = util.postData( pipeurl, pipeDataCallback, pipeEndCallback );
+		function pipeDataCallback( data ) {
+			res.write( data );
+		}
+		function pipeEndCallback() {
+			res.end();
+		}
+	}
+	else {
+		// in the case we don't have any further commands in
+		// the pipeline, we loop the command to write back
+		sys.puts( 'last node in the pipeline' );
+		var outrequest = res;
+	}
+	
 	var route = getRoute( req.url );
 	if( route == 'jath' ) {
 		doJath();
@@ -32,19 +54,19 @@ http.createServer( function( req, res ) {
 	}
 
 	function doJath() {
-		jathparse.parse( req, res );			
+		jathparse.parse( req, outrequest );			
 	}
 
 	function doUnix() {
 		var cmd = unixcmd.parseUnixUrl( req.url );
 		if( cmd.stdin ) {
 			sys.puts( 'stdin is from http get' );
-			var command = unixcmd.unixCommand( req, res, cmd.name, cmd.args, false );
+			var command = unixcmd.unixCommand( req, outrequest, cmd.name, cmd.args, false );
 			unixcmd.getStdinUrl( cmd.stdin, command );
 		}
 		else {
 			sys.puts( 'stdin is from http post' );
-			var command = unixcmd.unixCommand( req, res, cmd.name, cmd.args, true );
+			var command = unixcmd.unixCommand( req, outrequest, cmd.name, cmd.args, true );
 		}
 	}
 
@@ -53,4 +75,15 @@ http.createServer( function( req, res ) {
 function getRoute( url ) {
 	var path = require('url').parse( url ).pathname.split('/')[1];
 	return path;
+}
+// return an array of all other commands in the pipeline
+function getCommands( url ) {
+	var pipe = require('url').parse( url, true ).query.pipe;
+	sys.puts( 'pipeline urls: ' + decodeURI( pipe ) );
+	if( pipe ) {
+		return decodeURI( pipe ).split('|');
+	}	
+	else {
+		return null;
+	}
 }
